436023805a42904bc08e83d8cac994e5fe8bbc72
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2022-07-07 05:05:23 +0500

    financy first deploy

---
+++ precache-manifest.c674238ee267183e49e1a268af84ba81.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12d6a6217cdbb0145121",
+    "url": "/css/about.28858b87.css"
+  },
+  {
+    "revision": "f1b7df2668d3403810ae",
+    "url": "/css/app.24470b68.css"
+  },
+  {
+    "revision": "fac10ae1b85997b9ddf3",
+    "url": "/css/chunk-vendors.1474e5be.css"
+  },
+  {
+    "revision": "e5f18cb987385760e628a9671f975412",
+    "url": "/fonts/Inter-Medium.e5f18cb9.ttf"
+  },
+  {
+    "revision": "851660f90f21dba5ec35b1765fdd426a",
+    "url": "/fonts/Inter-Regular.851660f9.ttf"
+  },
+  {
+    "revision": "a2c4e8821556fa8b48d943a39f9da10c",
+    "url": "/fonts/Inter-SemiBold.a2c4e882.ttf"
+  },
+  {
+    "revision": "2736d3933f2182ae760a85f8cfc70065",
+    "url": "/img/logosport.2736d393.png"
+  },
+  {
+    "revision": "8118b9ea4f5e60c2b4206c506b0c09b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12d6a6217cdbb0145121",
+    "url": "/js/about.bf626546.js"
+  },
+  {
+    "revision": "f1b7df2668d3403810ae",
+    "url": "/js/app.c1dcee0a.js"
+  },
+  {
+    "revision": "fac10ae1b85997b9ddf3",
+    "url": "/js/chunk-vendors.d20e5a09.js"
+  },
+  {
+    "revision": "8cd46e75dd45317b5c2b8154ea64780d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
