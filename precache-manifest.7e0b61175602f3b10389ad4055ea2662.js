5ee22bf7ebd560c914abbec838a7ae550a1cd760
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2022-07-15 16:44:25 +0500

    fix width

--- precache-manifest.7e0b61175602f3b10389ad4055ea2662.js
+++ precache-manifest.7e0b61175602f3b10389ad4055ea2662.js
@@ -4,8 +4,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/css/about.28858b87.css"
   },
   {
-    "revision": "74a8995da826a0b022ba",
-    "url": "/css/app.0b9149c3.css"
+    "revision": "b7c2a5e33ee56dd53e6b",
+    "url": "/css/app.c7004a85.css"
   },
   {
     "revision": "9984236036db2eb1cfbb",
@@ -36,7 +36,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/img/malumot.476b7748.png"
   },
   {
-    "revision": "02277b031832bdaba2b2b68b0b2e58c1",
+    "revision": "39f919c3103e8e4a417c984ebd0f0a5b",
     "url": "/index.html"
   },
   {
@@ -44,7 +44,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/js/about.05c91962.js"
   },
   {
-    "revision": "74a8995da826a0b022ba",
+    "revision": "b7c2a5e33ee56dd53e6b",
     "url": "/js/app.0796dd25.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8cecda51c81253058cabf268db17182.js"
+  "/precache-manifest.7e0b61175602f3b10389ad4055ea2662.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "financy"});
